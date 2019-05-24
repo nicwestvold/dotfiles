cd43c61fc457857a1b91f337c71a8f97dd80eaf3
Nic Westvold <nwestvold@powerchordsystem.com>
2019-05-23 23:03:00 -0400

    updates flutter script

--- scripts/flutter.sh
+++ scripts/flutter.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo apt-get update
-sudo apt-get install -y  xz-utils libglu1-mesa
+sudo apt-get install -y xz-utils libglu1-mesa lib32stdc++6
 
 cd $HOME/Downloads
 curl -O https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v1.5.4-hotfix.2-stable.tar.xz
@@ -14,3 +14,10 @@ export PATH="$PATH:`pwd`/flutter/bin"
 
 # pre-download development binaries
 flutter precache
+
+# android studio - https://developer.android.com/studio
+cd /tmp
+curl -O https://dl.google.com/dl/android/studio/ide-zips/3.4.1.0/android-studio-ide-183.5522156-linux.tar.gz
+sudo tar zxvf android-studio-ide-183.5522156-linux.tar.gz -C /opt/
+# run the setup script to install Android Studio
+/opt/android-studio/bin/studio.sh
