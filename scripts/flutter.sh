816a7937a137e9f6546fc99ab9ea5f1ea02986ed
Nic Westvold <nwestvold@powerchordsystem.com>
2019-05-23 23:09:08 -0400

    adds dependencies to the flutter script

--- scripts/flutter.sh
+++ scripts/flutter.sh
@@ -16,6 +16,8 @@ export PATH="$PATH:`pwd`/flutter/bin"
 flutter precache
 
 # android studio - https://developer.android.com/studio
+sudo apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
+
 cd /tmp
 curl -O https://dl.google.com/dl/android/studio/ide-zips/3.4.1.0/android-studio-ide-183.5522156-linux.tar.gz
 sudo tar zxvf android-studio-ide-183.5522156-linux.tar.gz -C /opt/
