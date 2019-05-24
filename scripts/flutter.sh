ce56e1c574118ab9811557007c0813a29e8ba425
Nic Westvold <nwestvold@powerchordsystem.com>
2019-05-23 22:42:21 -0400

    adds flutter to PATH

--- scripts/flutter.sh
+++ scripts/flutter.sh
@@ -11,8 +11,6 @@ tar xf ~/Downloads/flutter_linux_v1.5.4-hotfix.2-stable.tar.xz
 
 # add `flutter` to the PATH
 export PATH="$PATH:`pwd`/flutter/bin"
-# add `flutter' to the .zshrc file
-echo "PATH=\"$PATH:$HOME/flutter/bin\"" >> $HOME/.zshrc
 
 # pre-download development binaries
 flutter precache
